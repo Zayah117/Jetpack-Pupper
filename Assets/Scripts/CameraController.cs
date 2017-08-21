c65f9797c3285fa622895d4ecbcc982ded989586
Zayah117 <isaiah.baker@simpledove.com>
2017-08-21 12:24:16 -0400

    Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform player;
+
+    private float yOffSet = 2.5f;
+
+	void Update () {
+        transform.position = new Vector3(transform.position.x, player.position.y + yOffSet, transform.position.z);
+	}
+}
