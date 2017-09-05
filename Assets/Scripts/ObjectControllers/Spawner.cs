e67105069d57dfb089f64a0488ca4081adfb9e4a
Zayah117 <isaiah.baker@simpledove.com>
2017-09-05 17:02:32 -0400

    Refactor Spawner script

--- Assets/Scripts/ObjectControllers/Spawner.cs
+++ Assets/Scripts/ObjectControllers/Spawner.cs
@@ -3,38 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
-    public float spawnAreaX;
-    public float targetAreaX;
     public float minSpawnRate;
     public float maxSpawnRate;
-    public float spawnTriggerSpeed;
-    public float fuelSpawnChance;
-    public AsteroidController asteroid;
-    public GameObject fuel;
+    public GameObject asteroid;
+    public GameObject[] powerups;
 
-    float fuelSpawnChanceIncrease = 0;
-    float fuelAreaX = 2.5f;
-    Vector3 target;
-    AsteroidController currentAsteroid;
+    float spawnRate;
+    float nextSpawn;
+    //float spawnAreaX = 3.5f;
+    //float targetAreaX = 3.5f;
+    //float powerupAreaX = 2.5f;
+    //Vector3 target;
+    //GameObject currentAsteroid;
 
-    public void SpawnObject() {
-        SpawnAsteroid();
+    void Start() {
+        spawnRate = NewSpawnRate();
+    }
 
-        if (Random.Range(0.0f, 1.0f) <= fuelSpawnChance + fuelSpawnChanceIncrease) {
-            SpawnFuel();
-        } else {
-            fuelSpawnChanceIncrease += 0.01f;
+    void Update() {
+        nextSpawn += Time.deltaTime * GameController.instance.speedMultiplier;
+        if (nextSpawn > spawnRate) {
+            spawnRate = NewSpawnRate();
+            nextSpawn = 0;
+            SpawnObject(asteroid);
         }
     }
 
+    void SpawnObject(GameObject selectedObject) {
+        // Spawn
+        GameObject currentObject = Instantiate(selectedObject, new Vector3(Random.Range(-3.0f, 3.0f), transform.position.y, 0), Quaternion.identity);
+        // Rotate
+        currentObject.GetComponent<ISpawnable>().RotateTowards(new Vector3(Random.Range(-3.0f, 3.0f), transform.GetChild(0).transform.position.y));
+    }
+
+    float NewSpawnRate() {
+        return Random.Range(minSpawnRate, maxSpawnRate);
+    }
+
+    /*
     void SpawnAsteroid() {
         currentAsteroid = Instantiate(asteroid, new Vector3(Random.Range(-spawnAreaX / 2, spawnAreaX / 2), transform.position.y, 0), Quaternion.identity);
         target = new Vector3(Random.Range(-targetAreaX / 2, targetAreaX / 2), transform.GetChild(0).transform.position.y);
-        currentAsteroid.RotateTowards(target);
+        currentAsteroid.GetComponent<ISpawnable>().RotateTowards(target);
     }
 
     void SpawnFuel() {
         Instantiate(fuel, new Vector3(Random.Range(-fuelAreaX, fuelAreaX), transform.position.y, 0), Quaternion.identity);
         fuelSpawnChanceIncrease = 0;
     }
+    */
 }
