a92e84e71e301c3b047b784dcaa6fec067efcaff
Zayah117 <isaiah.baker@simpledove.com>
2017-09-05 19:34:46 -0400

    Hud changes

---
+++ Assets/Scripts/UI/AmmoBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBar : MonoBehaviour {
+    public PupperController pupper;
+
+    RectTransform rectTransform;
+
+    void Start() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update() {
+        float barScale = (float)(pupper.ammo / (pupper.maxAmmo / 100)) / 100.0f;
+        rectTransform.localScale = new Vector3(1, barScale);
+    }
+}
