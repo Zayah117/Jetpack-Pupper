258fc38e915f790aa973b5765e770ac97ff41508
Isaiah Baker <isaiah.baker@simpledove.com>
2017-10-27 15:01:56 -0400

    Menu updates

---
+++ Assets/Scripts/Functionality/MenuNavigator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuNavigator : MonoBehaviour {
+	public void OpenMenu(GameObject openPanel) {
+		openPanel.SetActive(true);
+		this.transform.parent.gameObject.SetActive(false);
+	}
+}
