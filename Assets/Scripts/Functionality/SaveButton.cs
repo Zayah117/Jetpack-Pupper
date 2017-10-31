0f2fac810cdc2c9379436a57a86115b0cc26a034
Isaiah Baker <isaiah.baker@simpledove.com>
2017-10-31 16:27:19 -0400

    Button Updates

--- Assets/Scripts/Functionality/SaveButton.cs
+++ Assets/Scripts/Functionality/SaveButton.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SaveButton : MonoBehaviour {
+	public AudioSource backgroundMusicSource;
 	public Slider soundSlider;
 	public Slider musicSlider;
 
 	public void SaveVolume() {
 		PlayerPrefs.SetFloat("SFXVolume", soundSlider.value);
 		PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+		backgroundMusicSource.volume = PlayerPrefs.GetFloat("MusicVolume", 0.5f) * 0.5f;
 	}
 }
